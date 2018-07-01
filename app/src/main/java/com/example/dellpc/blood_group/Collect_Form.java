ec6435d838433a5b99c9b1633467eb4863629270
Arun Padmanabhan <arun.padmanabhan00@gmail.com>
2018-07-02 00:52:42 +0530

    Updates
    Fixed errors.

--- app/src/main/java/com/example/dellpc/blood_group/Collect_Form.java
+++ app/src/main/java/com/example/dellpc/blood_group/Collect_Form.java
@@ -14,33 +14,31 @@ public class Collect_Form extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collect__form);
+
+
+        final Button submit = (Button) findViewById(R.id.Submit);
+
+            submit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // add intent //
+                }
+            });
+
+        final EditText nameField = (EditText) findViewById(R.id.EditTextName);
+        String name = nameField.getText().toString();
+
+        final EditText emailField = (EditText) findViewById(R.id.EditTextEmail);
+        String email = emailField.getText().toString();
+
+        final EditText feedbackField = (EditText) findViewById(R.id.EditTextFeedbackBody);
+        String address = feedbackField.getText().toString();
+
+        final Spinner feedbackSpinner = (Spinner) findViewById(R.id.SpinnerFeedbackType);
+        String feedbackType = feedbackSpinner.getSelectedItem().toString();
+
+        final CheckBox responseCheckbox = (CheckBox) findViewById(R.id.CheckBoxResponse);
+        boolean bRequiresResponse = responseCheckbox.isChecked();
     }
-
-    final Button submit = (Button)findViewById(R.id.Submit);
-
-    public void sendForm(View button) {
-        submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // add intent //
-            }
-        });
-    }
-
-    final EditText nameField = (EditText) findViewById(R.id.EditTextName);
-    String name = nameField.getText().toString();
-
-    final EditText emailField = (EditText) findViewById(R.id.EditTextEmail);
-    String email = emailField.getText().toString();
-
-    final EditText feedbackField = (EditText) findViewById(R.id.EditTextFeedbackBody);
-    String address = feedbackField.getText().toString();
-
-    final Spinner feedbackSpinner = (Spinner) findViewById(R.id.SpinnerFeedbackType);
-    String feedbackType = feedbackSpinner.getSelectedItem().toString();
-
-    final CheckBox responseCheckbox = (CheckBox) findViewById(R.id.CheckBoxResponse);
-    boolean bRequiresResponse = responseCheckbox.isChecked();
-
 
 }
--- app/src/main/java/com/example/dellpc/blood_group/MainActivity.java
+++ app/src/main/java/com/example/dellpc/blood_group/MainActivity.java
@@ -13,13 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button donate_b = (Button)findViewById(R.id.donate_b);
+       /* final Button donate_b = (Button)findViewById(R.id.donate_b);
         final Button collect_b = (Button)findViewById(R.id.collect_b);
         final Button signup_b = (Button)findViewById(R.id.signup_b);
         final Button signin_b = (Button)findViewById(R.id.signin_b);
         final Button search_b = (Button)findViewById(R.id.search_b);
+*/
 
-    }
 
     final Button donate_b = (Button)findViewById(R.id.donate_b);
     final Button collect_b = (Button)findViewById(R.id.collect_b);
@@ -27,51 +27,51 @@ public class MainActivity extends AppCompatActivity {
     final Button signin_b = (Button)findViewById(R.id.signin_b);
     final Button search_b = (Button)findViewById(R.id.search_b);
 
-    public void OnClickButtonListener() {
-        donate_b.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent("com.example.dellpc.blood_group.MainActivity.Donate_Form");
-                        startActivity(intent);
-                    }
-                }
-        );
 
-        collect_b.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent2 = new Intent("com.example.dellpc.blood_group.MainActivity.Collect_Form");
-                        startActivity(intent2);
-                    }
+    donate_b.setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(getApplicationContext(),Donate_Form.class);
+                    startActivity(i);
                 }
-        );
+            }
+    );
 
-        signup_b.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-                                            Intent intent3 = new Intent("com.example.dellpc.blood_group.MainActivity.Sign_Up");
-                                            startActivity(intent3);
-                                        }
+    collect_b.setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(getApplicationContext(),Collect_Form.class);
+                    startActivity(i);
+                }
+            }
+    );
+
+    signup_b.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        Intent i = new Intent(getApplicationContext(),Sign_Up.class);
+                                        startActivity(i);
                                     }
-        );
+                                }
+    );
 
-        signin_b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent4 = new Intent("com.example.dellpc.blood_group.MainActivity.Sign_In");
-                startActivity(intent4);
-            }
-        });
+    signin_b.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(getApplicationContext(),Sign_In.class);
+            startActivity(i);
+        }
+    });
 
-        search_b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent5 = new Intent("com.example.dellpc.blood_group.MainActivity.Search");
-                startActivity(intent5);
-            }
-        });
+    search_b.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(getApplicationContext(),Search.class);
+            startActivity(i);
+        }
+    });
 
     }
 
